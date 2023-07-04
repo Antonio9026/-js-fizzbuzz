44090e5d852548a41f1782fd9f7eb687e7b93d4b
Antonio Piras <antonio.piras90@gmail.com>
2023-07-04 15:30:55 +0200

    cicloi numeri da 1 a 100 +condizione multipli

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+// // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// 1. Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// 2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo “a mano”
+
+// **BONUS 1:**
+// Crea un container nel DOM , aggiungendo (attraverso la funzione `append()`) un elemento html con il numero o la stringa corretta da mostrare.
+// **BONUS 2:**
+// Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+
+
+
+
+// creo un ciclo per stampare in console i numeri da 1 a 100
+
+for (let i = 0; i <= 100; i++) {
+    console.log(i);
+
+    // creo condizione per stampare in console se multipli di 3 "fizz",se multipli di 5 "buzz",se multipli di 15 "fizzbuzz",
+    // creo condizione per stampare in console se multipli di 3
+   if (i % 3 === 0) {
+    console.log("fizz");
+   }else if (i % 5 === 0) {
+    console.log("buzz");
+   }else if (i % 15 === 0) {
+    console.log("fizzbuzz");
+   }
+   
+}

7469b07e94cfea00487a266c90741862be7ba300
Antonio Piras <antonio.piras90@gmail.com>
2023-07-04 17:07:54 +0200

    aggiungo lo stile alla pagina /no responsive

--- JS/script.js
+++ JS/script.js
@@ -11,21 +11,34 @@
 // Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 
 
-
-
+let container = document.getElementById("container")
+const box = 10 * 10;
 // creo un ciclo per stampare in console i numeri da 1 a 100
 
-for (let i = 0; i <= 100; i++) {
-    console.log(i);
+for (let i = 1; i <= 100; i++) {
+   const box = document.createElement("div");
+   box.classList.add("child")
 
     // creo condizione per stampare in console se multipli di 3 "fizz",se multipli di 5 "buzz",se multipli di 15 "fizzbuzz",
     // creo condizione per stampare in console se multipli di 3
-   if (i % 3 === 0) {
-    console.log("fizz");
+   if (i % 15 === 0) {
+    box.innerHTML += "fizzbuzz";
+    box.style.borderRadius = "20px  20px 0"
+    box.style.backgroundColor = "#DDA0DD"
    }else if (i % 5 === 0) {
-    console.log("buzz");
-   }else if (i % 15 === 0) {
-    console.log("fizzbuzz");
+    box.innerHTML +="buzz";
+    box.style.borderRadius = "0 20px  20px "
+    box.style.backgroundColor = "#4B0082"
+   }else if (i % 3 === 0) {
+    box.innerHTML += "fizz";
+    box.style.borderRadius = " 20px 0 20px "
+    box.style.backgroundColor = "#8A2BE2"
+   }else{
+    box.innerHTML += (i);
+    box.style.borderRadius = " 30px  20px 10px"
+    box.style.backgroundColor = "#FF00FF"
+    
    }
-   
+ 
+   container.append(box)
 }
